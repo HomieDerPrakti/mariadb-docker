9f8f8a7bd04aba21871c36f08853b9728061cbf5
HomieDerPrakti <julian.elias.egger@outlook.com>
2026-01-14 16:18:52 +0100

    updated both scripts with safety checks and correct logging

--- full-backup.sh
+++ full-backup.sh
@@ -2,15 +2,19 @@
 
 set -euo pipefail
 
-sudo mount /mnt/backup
-
-echo "[$(date +%H:%M:%S)] Mounting NFS share"
-
-BACKUP_ROOT=/mnt/backup/
 DATE=$(date +%Y-%m-%d)
+BACKUP_ROOT=/mnt/backup
+LOG="${BACKUP_ROOT}/log-full-${DATE}.txt"
+
+echo "[$(date +%H:%M:%S)] Mounting NFS share..."
+{ 
+    findmnt -t nfs,nfs4 /mnt/backup >/dev/null && echo "[$(date +%H:%M:%S)] NFS share already mounted." | tee -a "$LOG"; 
+} || { 
+    sudo mount /mnt/backup && echo "[$(date +%H:%M:%S)] NFS share successfully mounted." | tee -a "$LOG"; 
+}
+
 week_dir="${BACKUP_ROOT}/week-${DATE}"
 DEFAULTS=/var/mariadb-backup/.my.cnf
-LOG="${BACKUP_ROOT}/log-full-${DATE}"
 backup_ok=0
 
 num=1
@@ -28,20 +32,28 @@ fi
 
 TARGET_DIR="${week_dir}/full-${DATE}"
 
-mkdir -p $TARGET_DIR
-
 cleanup() {
-    if findmnt -t nfs,nfs4 /mnt/backup >/dev/null; then
+    if findmnt -t nfs,nfs4 /mnt/backup >/dev/null 2>&1; then
         if [[ $backup_ok == "0" ]]; then
             echo "[$(date +%H:%M:%S)] cleaning up files..." | tee -a "$LOG"
-            rm -rf "${WEEK_DIR}/"
+            [[ -n "$week_dir" && "$week_dir" == "$BACKUP_ROOT"/week-* ]] && rm -rf "${week_dir}/"
+            mkdir -p "${BACKUP_ROOT}/error-logs"
+            mv "$LOG" "${BACKUP_ROOT}/error-logs/error-log-full-${DATE}.txt"
+            echo "[$(date +%H:%M:%S)] Unmounting NFS share..." | tee -a "${BACKUP_ROOT}/error-logs/error-log-full-${DATE}.txt"
+            sudo umount /mnt/backup
+            return
+        else
+            mkdir -p "${week_dir}/logs"
+            mv "$LOG" "${week_dir}/logs/log-full-${DATE}.txt"
+            echo "[$(date +%H:%M:%S)] unmonting NFS share" | tee -a "${week_dir}/logs/log-full-${DATE}.txt"
+            sudo umount /mnt/backup
         fi
-        echo "[$(date +%H:%M:%S)] unmonting NFS share" | tee -a "$LOG"
-        sudo umount /mnt/backup
     fi
 }
 
 trap cleanup EXIT
+
+mkdir -p "$TARGET_DIR"
 
 echo "[$(date +%H:%M:%S)] Starting Backup to $TARGET_DIR" | tee -a "$LOG"
 
@@ -63,4 +75,3 @@ mariadb-backup \
 backup_ok=1
 
 echo "[$(date +%H:%M:%S)] Done" | tee -a "$LOG"
-
--- incremental-backup.sh
+++ incremental-backup.sh
@@ -1,50 +1,65 @@
 #!/bin/bash
 
 set -euo pipefail
+DEFAULTS="/var/mariadb-backup/.my.cnf"
 BACKUP_ROOT=/mnt/backup
 DATE="$(date +%Y-%m-%d)"
-LOG="${BACKUP_ROOT}/log-inc-${DATE}"
+LOG="${BACKUP_ROOT}/log-inc-${DATE}.txt"
+backup_ok=0
+target_dir=""
+LATEST_WEEK=""
 
 echo "[$(date +%H:%M:%S)] Mounting NFS share..."
-sudo mount /mnt/backup
-
-LATEST_WEEK=$(find "$BACKUP_ROOT" -maxdepth 1 -type d -name 'week-*' | sort | tail -n 1 )
-target_dir="${LATEST_WEEK}/inc-${DATE}"
-DEFAULTS="/var/mariadb-backup/.my.cnf"
-backup_ok=0
+{ 
+    findmnt -t nfs,nfs4 /mnt/backup >/dev/null 2>&1 && echo "[$(date +%H:%M:%S)] NFS share already mounted."; 
+} || { 
+    sudo mount /mnt/backup && echo "[$(date +%H:%M:%S)] NFS share successfully mounted."; 
+}
 
 cleanup() {
-    if findmnt -t nfs,nfs4 /mnt/backup >/dev/null; then
-        if [[ $backup_ok == "0" ]]; then
-            echo "[$(date +%H:%M:%S)] Cleaning up files..." | tee -a $LOG
-            rm -rf $target_dir
-            echo "[$(date +%H:%M:%S)] Unmounting NFS share..." | tee -a $LOG
+    if findmnt -t nfs,nfs4 /mnt/backup >/dev/null 2>&1; then
+        if [[ $backup_ok == "0" || -z "$LATEST_WEEK" ]]; then
+            echo "[$(date +%H:%M:%S)] Cleaning up files..." | tee -a "$LOG"
+            [[ ! -z "$target_dir" ]] && rm -rf "$target_dir"
+            mkdir -p "${BACKUP_ROOT}/error-logs/"
+            mv "$LOG" "${BACKUP_ROOT}/error-logs/error-log-inc-${DATE}.txt"
+            echo "[$(date +%H:%M:%S)] Unmounting NFS share..." | tee -a "${BACKUP_ROOT}/error-logs/error-log-inc-${DATE}.txt"
             sudo umount /mnt/backup
-            exit 1
+            return
+        else
+            mkdir -p "${LATEST_WEEK}/logs"
+            mv "$LOG" "${LATEST_WEEK}/logs/log-inc-${DATE}.txt"
+            echo "[$(date +%H:%M:%S)] Unmounting NFS share..." | tee -a "${LATEST_WEEK}/logs/log-inc-${DATE}.txt"
+            sudo umount /mnt/backup
         fi
-        echo "[$(date +%H:%M:%S)] Unmounting NFS share..." | tee -a $LOG
-        sudo umount /mnt/backup
     fi
 }
 
 trap cleanup EXIT
 
+LATEST_WEEK=$(find "$BACKUP_ROOT" -maxdepth 1 -type d -name 'week-*' 2>/dev/null | sort -V | tail -n 1 )
+[[ -z "$LATEST_WEEK" ]] && {
+    echo "[$(date +%H:%M:%S)] No Week directory found. Please make a full backup first"
+    exit 1
+}
+target_dir="${LATEST_WEEK}/inc-${DATE}"
+
 last_base="$(
-    find "$LATEST_WEEK" -maxdepth 1 -type d -name 'inc-*' | sort | tail -n 1 || true
+    find "$LATEST_WEEK" -maxdepth 1 -type d -name 'inc-*' 2>/dev/null | sort -V | tail -n 1 || true
 )"
 
-if [[ -z $last_base ]]; then
-    last_base=$(
-        find "$LATEST_WEEK" -maxdepth 1 -type d -name 'full-*' | sort | tail -n 1 || true
-    )
+if [[ -z "$last_base" ]]; then
+    last_base="$(
+        find "$LATEST_WEEK" -maxdepth 1 -type d -name 'full-*' 2>/dev/null | sort -V | tail -n 1 || true
+    )"
 fi
 
-if [[ -z $last_base ]]; then
-    echo "[$(date +%H:%M:%S)] No backup base found. Make a full backup first." | tee -a $LOG
+if [[ -z "$last_base" ]]; then
+    echo "[$(date +%H:%M:%S)] No backup base found. Make a full backup first." | tee -a "$LOG"
     exit 1
 fi
 
-if [[ -e $target_dir ]]; then
+if [[ -e "$target_dir" ]]; then
     num=1
     while :; do 
         if [[ -e "${target_dir}_${num}" ]]; then
@@ -57,17 +72,17 @@ if [[ -e $target_dir ]]; then
     done
 fi
 
-echo "[$(date +%H:%M:%S)] Starting INCREMENTAL backup to $target_dir" | tee -a $LOG
-echo "[$(date +%H:%M:%S)] Based on: $last_base"
+echo "[$(date +%H:%M:%S)] Starting INCREMENTAL backup to $target_dir" | tee -a "$LOG"
+echo "[$(date +%H:%M:%S)] Based on: $last_base" | tee -a "$LOG"
 
 mariadb-backup \
-    --defaults-extra-file=$DEFAULTS \
+    --defaults-extra-file="$DEFAULTS" \
     --backup \
-    --target-dir=$target_dir \
-    --incremental-basedir=$last_base \
-    --history=$DATE \
+    --target-dir="$target_dir" \
+    --incremental-basedir="$last_base" \
+    --history="$DATE" \
     2>&1 | tee -a "$LOG"
 
-echo "[$(date +%H:%M:%S)] Incremental backup OK."
+echo "[$(date +%H:%M:%S)] Incremental backup OK." | tee -a "$LOG"
 backup_ok=1
-echo "[$(date +%H:%M:%S)] Done"
+echo "[$(date +%H:%M:%S)] Done" | tee -a "$LOG"
